eff692cb7cf704bb54fb0bd9a8adf1205b5027cf
Melvin Antony <melvinantony000@gmail.com>
2022-07-27 12:20:57 +0530

    ninth_commit

--- src/AnimeDetails/AnimeDetails.js
+++ src/AnimeDetails/AnimeDetails.js
@@ -17,11 +17,11 @@ const AnimeDetails = ()=>{
         const updated_data=JSON.parse(localStorage.getItem("watchlist"));
         updated_data.push(res)
 
-        // const remv_dup = updated_data.filter((item,index)=>{
-        //     return updated_data.findIndex(obj=>obj.id === item.id) === index
-        //     })
+        const remv_dup = updated_data.filter((item,index)=>{
+            return updated_data.findIndex(obj=>obj.mal_id === item.mal_id) === index
+            })
 
-       localStorage.setItem("watchlist",JSON.stringify(updated_data))
+       localStorage.setItem("watchlist",JSON.stringify(remv_dup))
     
         dispatch({
             type:"disabled",
